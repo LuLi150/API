0d3b8d8504df64d7e19f4581050c57ebd221aaa7
LuLi150 <luabranca.0987@gmail.com>
2025-06-06 16:33:22 -0300

    Add files via upload

--- backend/node/src/middlewares/routeHandler.js
+++ backend/node/src/middlewares/routeHandler.js
@@ -1,4 +1,7 @@
-import { routes } from "../routes.js"
+import { routes } from "../routes.js";
+import { Database } from "../database.js";
+
+const database = new Database();
 
 export const routeHandler = (request, response) => {
 
@@ -12,11 +15,8 @@ export const routeHandler = (request, response) => {
     const { ...params } = routeParams.groups;
 
    request.params = params;
-
-   console.log(routeParams);
    
-
-    return route.controller(request, response);
+    return route.controller({request, response, database});
   }
 
   return response.writeHead(400).end("Erro! O servido não pôde processa a requisição")
